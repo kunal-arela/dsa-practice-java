479b852b0a037bea59c97a942d56fb4f9362a43c
Kunal Arela <arelakunal238@gmail.com>
2026-03-04 00:06:08 +0530

    Improved documentation for Two Sum

--- arrays/TwoSum.java
+++ arrays/TwoSum.java
@@ -1,13 +1,23 @@
+/**
+ * Problem: Two Sum
+ * Approach: Brute Force
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(1)
+ */
 class TwoSum {
+
     public static int[] twoSum(int[] nums, int target) {
+
         for (int i = 0; i < nums.length; i++) {
+
             for (int j = i + 1; j < nums.length; j++) {
+
                 if (nums[i] + nums[j] == target) {
                     return new int[]{i, j};
                 }
             }
         }
-        return new int[]{-1, -1};
+
+        return new int[]{-1, -1}; 
     }
 }
-//Added Two Sum problem
