a427cedca27f37a726af6b695e014e0c009e4a15
Kunal Arela <arelakunal238@gmail.com>
2026-03-04 00:06:56 +0530

    Improved documentation for Maximum Subarray

--- arrays/MaximumSubarray.java
+++ arrays/MaximumSubarray.java
@@ -1,10 +1,18 @@
+/**
+ * Problem: Maximum Subarray
+ * Algorithm: Kadane's Algorithm
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
 class MaximumSubarray {
 
     public static int maxSubArray(int[] nums) {
+
         int maxSum = nums[0];
         int currentSum = nums[0];
 
         for (int i = 1; i < nums.length; i++) {
+
             currentSum = Math.max(nums[i], currentSum + nums[i]);
             maxSum = Math.max(maxSum, currentSum);
         }
