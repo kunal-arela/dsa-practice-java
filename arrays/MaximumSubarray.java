d8e5d8ec721648808d5dcaff5c27975c1d1a4002
Kunal Arela <arelakunal238@gmail.com>
2026-03-16 01:41:23 +0530

    Optimal MaximumSubarray.java

--- arrays/MaximumSubarray.java
+++ arrays/MaximumSubarray.java
@@ -4,19 +4,29 @@
  * Time Complexity: O(n)
  * Space Complexity: O(1)
  */
-class MaximumSubarray {
-
-    public static int maxSubArray(int[] nums) {
-
-        int maxSum = nums[0];
-        int currentSum = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-
-            currentSum = Math.max(nums[i], currentSum + nums[i]);
-            maxSum = Math.max(maxSum, currentSum);
+import java.util.*;
+public class subarray{
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        int size = input.nextInt();
+        int [] nums = new int[size];
+        for(int i = 0;i<size;i++){
+            nums[i] = input.nextInt();
         }
-
-        return maxSum;
+        System.out.println(maxsubarray(nums));
+    }
+    public static int maxsubarray(int [] nums){
+        int n = nums.length;
+        int maxsum = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i = 0;i<n;i++){
+            sum += nums[i];
+            if(maxsum < sum ){
+                maxsum = sum;
+            }if(sum < 0){
+                sum = 0;
+            }
+        }
+        return maxsum;
     }
 }
