604034eae74093bc5260e023c9b26c431232b43f
Kunal Arela <arelakunal238@gmail.com>
2026-06-24 01:26:21 +0530

    Create  numOfSubarrays.java

---
+++ arrays/numOfSubarrays.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int n = arr.length;
+        int sum = 0;
+        int count = 0;
+        int target = k * threshold;
+        for(int i = 0;i<k;i++){
+            sum += arr[i];
+        }
+        if(sum >= target){
+            count++;
+        }
+        for(int i = k;i<n;i++){
+           sum +=  arr[i];
+           sum -= arr[i-k];
+           if(sum >= target){
+            count++;
+           }
+        }
+        return count;
+        
+    }
+}
